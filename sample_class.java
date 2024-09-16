02b994715e271afa9a239ce6dd60795162c278f3
rhdh-orchestrator-test <rhdh-orchestrator-test@redhat.com>
2024-09-16 17:02:21 +0300

    Create sample_class.java

---
+++ sample_class.java
@@ -0,0 +1,14 @@
+class Bicycle {
+
+  // state or field
+  private int gear = 5;
+
+  // behavior or method
+  public void braking() {
+    System.out.println("Braking");
+  }
+
+  public void moving() {
+    System.out.println("Moving");
+  }
+}
